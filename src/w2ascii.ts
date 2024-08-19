88f64ac950e9d7250fa35296db91d670926085ef
Henil Malaviya <henilm.dev@gmail.com>
2024-08-19 16:38:48 +0530

    feat: implement w2ascii function

--- src/w2ascii.ts
+++ src/w2ascii.ts
@@ -1,1 +1,7 @@
-// Waiting for henil0604 to implement this 😃!
+export function w2ascii(str: string): number {
+    let sum = 0;
+    for (let i = 0; i < str.length && str[i] !== ' '; i++) {
+        sum += str.charCodeAt(i);
+    }
+    return sum;
+}
